f87d718c1c406fc598e5f81813d64c58b1653ce4
KoinGG <supermatvey100@mail.ru>
2023-12-13 01:57:04 +0700

    final commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,11 @@ type RESPONSE_DATA = {
 };
 
 const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
-  const path = `http://localhost:3001/api/${endpoint}`;
+  const path =
+    process.env.NODE_ENV === 'development'
+      ? `http://localhost:3001/api/${endpoint}`
+      : `https://raw.githubusercontent.com/KoinGG/VIS-LR3/gh-pages/static/db/${endpoint}.json`;
+
   const response = await fetch(path);
 
   return await response.json();
